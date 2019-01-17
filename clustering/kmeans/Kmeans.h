2ed74814198a25aa4cdad1cba65522bfcba98d64
jerrykcode <jerrykang026@gmail.com>
2019-01-17 20:54:42 +0800

    Initial Commit -- add kmeans

---
+++ clustering/kmeans/Kmeans.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+using namespace std;
+
+typedef struct Point {	
+	double *data;
+	int dimension;
+	Point(double *data, int dimension) : data(data), dimension(dimension) {}
+} *PPoint;
+
+class Kmeans {
+public:
+	Kmeans(); 
+	~Kmeans();
+
+	void clustering(int nClusters, double *dataBuffer, int nRows, int nCols, int *label);
+private:
+	int nClusters;
+	int nRows;
+	int nCols;
+	vector<Point> dataSet;
+	vector<Point> centroids;
+
+	void loadBuffer(double *dataBuffer, int nRows, int nCols);
+	void kmeans_clustering(int *label);
+	void randomSetCentroids();
+	void clearData();
+};
